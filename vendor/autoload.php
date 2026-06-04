c01dcbeece20ab981192b8637082dc97801b0023
Socialforger <socialforger@gmail.com>
2026-06-04 21:19:59 +0200

    Update autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,26 +1,42 @@
 <?php
 /**
- * Custom Autoloader per Symfony ExpressionLanguage v8.2
- * Sostituisce l'autoloader di Composer per caricamento manuale.
+ * WP-ESG Manual Ecosytem Autoloader
+ * 
+ * Gestisce il caricamento automatico PSR-4 sia per le dipendenze interne di Symfony
+ * (gestendo il typo della cartella 'symphony/'), sia per le classi natie del plugin.
  */
 
-if (!defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
 }
 
-spl_autoload_register(function (string $class) {
-    $prefix = 'Symfony\\Component\\ExpressionLanguage\\';
-    $base_dir = __DIR__ . '/symfony/expression-language/';
+spl_autoload_register(function ($class) {
+    
+    // 1. NAMESPACE PRINCIPALE PLUGIN: WpEsg\ -> /includes/
+    $plugin_prefix = 'WpEsg\\';
+    $plugin_base_dir = dirname(__DIR__) . '/includes/';
 
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
+    if (strpos($class, $plugin_prefix) === 0) {
+        $relative_class = substr($class, strlen($plugin_prefix));
+        $file = $plugin_base_dir . str_replace('\\', '/', $relative_class) . '.php';
+        
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
     }
 
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    // 2. DIPENDENZA SYMFONY: Symfony\Component\ExpressionLanguage\ -> /vendor/symphony/...
+    $symfony_prefix = 'Symfony\\Component\\ExpressionLanguage\\';
+    $symfony_base_dir = __DIR__ . '/symphony/expression-language/';
 
-    if (file_exists($file)) {
-        require_once $file;
+    if (strpos($class, $symfony_prefix) === 0) {
+        $relative_class = substr($class, strlen($symfony_prefix));
+        $file = $symfony_base_dir . str_replace('\\', '/', $relative_class) . '.php';
+        
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
     }
 });
